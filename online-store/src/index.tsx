59026174aa2e4d00365ab88d18e84c1f65d78a77
Ben <jumpeebunee@gmail.com>
2022-12-14 09:52:09 +0300

    feat: add styles

--- online-store/src/index.tsx
+++ online-store/src/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
+import './styles/normolize.css';
+import './styles/global.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
