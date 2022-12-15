389913324ab874cb22a952442fc21547a20615a3
Ben <jumpeebunee@gmail.com>
2022-12-15 13:14:28 +0300

    fix: fix gap and add new product

--- online-store/src/app/feautures/productsSlice.ts
+++ online-store/src/app/feautures/productsSlice.ts
@@ -42,7 +42,31 @@ const initialState: MainProductsProps = {
             sale: 0,
             raiting: 3.4,
             stock: 23,
-            description: 'Hasami Porcelain blends modern design with the almost 400-year-old pottery tradition of the town of Hasami in Japan. These bowls feature an eye-catching cylindrical design with rounded inner edges and a gloss finish. Like all Hasami pieces, they are modular and stackable.'
+            description: 'Hasami Porcelain blends modern design with the almost 400-year-old pottery tradition of the town of Hasami in Japan. These plates feature a beautiful and stackable design with a gloss finish.'
+        },
+        {
+            id: 4,
+            categoryId: 2,
+            image: 'https://uploads-ssl.webflow.com/60feafdac3e33ed6180205f3/62a7453372553e3ece76d2a9_oxo-nylon-flexible-turner-p-800.png',
+            title: 'Nylon Flexible Turner',
+            brand: 'OXO',
+            price: 10,
+            sale: 15,
+            raiting: 5,
+            stock: 12,
+            description: 'The Nylon Flexible Turner is a non-stick safe spatula with a thin and flexible nylon head that is perfect for flipping all kinds of foods while keeping them intact. Like other products from the Good Grips family, the handle is soft, comfortable, and non-slip.'
+        },
+        {
+            id: 5,
+            categoryId: 2,
+            image: 'https://uploads-ssl.webflow.com/60feafdac3e33ed6180205f3/62a8c556837e3132715c56dd_wp-porter-bowl.png',
+            title: 'Porter Bowl',
+            brand: 'W&P',
+            price: 20,
+            sale: 0,
+            raiting: 4.7,
+            stock: 42,
+            description: 'The Porter Bowl is a simple and ultralight lunchbox that makes it easy to take your food wherever you go, whether that’s the office or the beach. The plastic base is wrapped with soft protective silicon, which makes it a joy to eat from. It’s also microwave and dishwasher safe.'
         },
     ],
 }
