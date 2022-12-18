e908d2632a735f622011953824540bd184d0d8a3
Ben <jumpeebunee@gmail.com>
2022-12-18 11:59:48 +0300

    feat: add scroll to top between pages

--- online-store/src/App.tsx
+++ online-store/src/App.tsx
@@ -1,8 +1,10 @@
 import AppNavigation from "./components/AppNavigation"
+import ScrollToTop from "./components/ScrollToTop"
 
 const App = () => {
   return (
     <main>
+      <ScrollToTop/>
       <AppNavigation/>
     </main>
   )
