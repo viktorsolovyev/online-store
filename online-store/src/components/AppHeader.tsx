ba2ced178b3978b9b53d962d6d6950abb4daa846
Viktor Solovyev <barn-mih@rambler.ru>
2022-12-20 17:12:38 +0300

    feat: add reactive total amount in header

--- online-store/src/components/AppHeader.tsx
+++ online-store/src/components/AppHeader.tsx
@@ -1,13 +1,20 @@
 import { Link } from "react-router-dom";
 import "../styles/components/header.css";
+import { RootState } from "../../src/app/store";
+import { useSelector } from "react-redux";
 
 const AppHeader = () => {
+  const totalAmount: number = useSelector(
+    (state: RootState) => state.cart.cart.length
+  );
 
   return (
     <header className="header__wrapper">
       <div className="header__header">
         <div className="header__logo">
-          <span>ONLINE</span>STORE
+          <Link to="/">
+            <span>ONLINE</span>STORE
+          </Link>
         </div>
         <form className="header__search-form" action="">
           <button type="submit" className="header__search-sumbit"></button>
@@ -16,9 +23,11 @@ const AppHeader = () => {
             type="search"
             placeholder="Find cat"
           />
-        </form>        
-        <Link to="/cart" className="header__cart">          
-          <div className="header__cart-count">20</div>
+        </form>
+        <Link to="/cart" className="header__cart">
+          {totalAmount > 0 &&
+            <div className="header__cart-count">{totalAmount}</div>
+          }
         </Link>
       </div>
     </header>
