09934303badba3778f5e9ac89b1034f09d2409ad
Ben <jumpeebunee@gmail.com>
2022-12-26 11:53:57 +0300

    feat: style 404 page

--- online-store/src/pages/ErrorPage.tsx
+++ online-store/src/pages/ErrorPage.tsx
@@ -1,6 +1,12 @@
+import '../styles/pages/errorPage.css';
+
 const ErrorPage = () => {
   return (
-    <div>ErrorPage</div>
+    <div className="container error__container">
+      <h2 className='error__heading'>No results found</h2>
+      <p className='error__description'>We couldn't find what you searched for. Try searching again.</p>
+      <button className='btn error__btn'>Home</button>
+    </div>
   )
 }
 
