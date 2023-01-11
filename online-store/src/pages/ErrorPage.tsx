600706b10aa32fdf9d3a9ac5d3ceaf955d27aff3
Ben <jumpeebunee@gmail.com>
2023-01-11 09:28:33 +0300

    fix: fix link btn

--- online-store/src/pages/ErrorPage.tsx
+++ online-store/src/pages/ErrorPage.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import '../styles/pages/errorPage.css';
 
 const ErrorPage = () => {
@@ -5,7 +6,7 @@ const ErrorPage = () => {
     <div className="container error__container">
       <h2 className='error__heading'>No results found</h2>
       <p className='error__description'>We couldn't find what you searched for. Try searching again.</p>
-      <button className='btn error__btn'>Home</button>
+      <Link to="/" className='btn error__btn'>Home</Link>
     </div>
   )
 }
