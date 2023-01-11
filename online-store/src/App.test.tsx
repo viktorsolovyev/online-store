8a5c48cb39c27c9441197920f5d6a09f8fadd2f7
Ben <jumpeebunee@gmail.com>
2023-01-11 09:22:54 +0300

    fix: fix routing

--- online-store/src/App.test.tsx
+++ online-store/src/App.test.tsx
@@ -3,12 +3,10 @@ import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
+describe("App", () => {
+  it("renders App component", () => {
+    render(<App/>);
+  })
+})
 
-  expect(getByText(/learn/i)).toBeInTheDocument();
-});
+ 
